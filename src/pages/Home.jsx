a2969de08847a485ba729ce13fb8533137c75b68
mustafa-3 <sagunmustafa14@gmail.com>
2022-10-05 18:58:08 +0300

    v4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,22 @@
-import React, { useEffect, useState } from "react";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Unstable_Grid2";
+import { useEffect, useState } from "react";
+import Cards from "../components/Cards";
 import Navbar from "../components/Navbar";
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
 const Home = () => {
-  const [recipeData, setRecipeData] = useState();
+  const [recipeData, setRecipeData] = useState([]);
   const [query, setQuery] = useState("chicken");
   const [selectedMeal, setSelectedMeal] = useState("lunch");
   const APP_KEY = "3cde9e853ac30b4dfd6e6971fa023393";
@@ -12,7 +26,7 @@ const Home = () => {
   const getData = () => {
     fetch(url)
       .then((res) => res.json())
-      .then((data) => setRecipeData(data))
+      .then((data) => setRecipeData(data.hits))
       .catch((err) => console.log(err));
   };
 
@@ -23,9 +37,20 @@ const Home = () => {
   console.log(recipeData);
 
   return (
-    <div>
-      {/* <Navbar /> */}
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={3}>
+        {recipeData.map((item, index) => {
+          return (
+            <Grid item key={index}>
+              <Item>
+                <Cards recipeData={item} />
+              </Item>
+              ;
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Box>
   );
 };
 
